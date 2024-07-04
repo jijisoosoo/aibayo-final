c68dff0f41d41ffff1b990d8331a3440402a351c
AlexMJY <jmoon0227@gmail.com>
2024-07-04 15:02:47 +0900

    first commit

---
+++ src/main/java/com/aibayo/aibayo/AibayoApplication.java
@@ -0,0 +1,13 @@
+package com.aibayo.aibayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AibayoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AibayoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aibayo/aibayo/AibayoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aibayo.aibayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibayoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
