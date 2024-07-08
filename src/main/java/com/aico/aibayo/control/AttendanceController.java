bb4d27a88131a73ceffef8fb08fc3d9693d5a258
Alex-MJY <jmoon0227@gmail.com>
2024-07-08 17:15:57 +0900

    MJY-출석부 디테일(오늘날짜), aibayo 아이콘 a tag 추가 (밑줄, 색변환 제거)

--- src/main/java/com/aico/aibayo/control/AttendanceController.java
+++ src/main/java/com/aico/aibayo/control/AttendanceController.java
@@ -1,9 +1,11 @@
 package com.aico.aibayo.control;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/attendance")
 public class AttendanceController {
@@ -11,5 +13,20 @@ public class AttendanceController {
     @GetMapping("/admin/main")
     public String adminMain() {
         return "attendance/admin/main";
+    }
+
+    @GetMapping("/admin/detailToday")
+    public void detailToday() {
+        log.info("detailToday");
+    }
+
+    @GetMapping("/admin/detailBefore")
+    public void detailBefore() {
+        log.info("detailBefore");
+    }
+
+    @GetMapping("/admin/detailAfter")
+    public void detailAfter() {
+        log.info("detailAfter");
     }
 }
