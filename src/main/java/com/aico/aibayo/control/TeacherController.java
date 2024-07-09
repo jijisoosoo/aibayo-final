3c1951c92a7659b4d2317ed474e7e935e16a18de
SMJ <mindyso7151@gmail.com>
2024-07-09 17:38:44 +0900

    SMJ-Teacher HTML 수정

--- src/main/java/com/aico/aibayo/control/TeacherController.java
+++ src/main/java/com/aico/aibayo/control/TeacherController.java
@@ -1,4 +1,22 @@
 package com.aico.aibayo.control;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/teacher")
 public class TeacherController {
+
+    @GetMapping("/admin/teacher")
+    public String adminMain() {
+        return "/teacherManage/main";
+    }
+
+
+
+
+
 }
