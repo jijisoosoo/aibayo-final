b9ac77680672973bfb008acb45b1db049aa0ce8d
AlexMJY <jmoon0227@gmail.com>
2024-07-09 10:26:58 +0900

    pr test

---
+++ src/main/java/com/aico/aibayo/control/ApiAttendanceController.java
@@ -0,0 +1,13 @@
+package com.aico.aibayo.control;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiAttendanceController {
+
+    @GetMapping("/at")
+    public void at() {
+        
+    }
+}
