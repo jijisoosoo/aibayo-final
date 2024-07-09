d7322e413becaf3087910791466fcd71813acd8b
JS <ji931229@gmail.com>
2024-07-09 11:10:54 +0900

    HJS-전체조회_구현

--- src/main/java/com/aico/aibayo/control/AnnounceController.java
+++ src/main/java/com/aico/aibayo/control/AnnounceController.java
@@ -1,7 +1,14 @@
 package com.aico.aibayo.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/announce")
 public class AnnounceController {
+    @GetMapping("/admin/list")
+    public String adminList(){
+        return "/announce/admin/list";
+    }
 }
