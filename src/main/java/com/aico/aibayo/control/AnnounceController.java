8e231ce1c032d040063ba94f93977cbe1583d55e
jisoo <ji931229@gmail.com>
2024-07-11 22:12:34 +0900

    HJS-announcePage_목록_카드형_리스트형_구현

--- src/main/java/com/aico/aibayo/control/AnnounceController.java
+++ src/main/java/com/aico/aibayo/control/AnnounceController.java
@@ -7,17 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/announce")
 public class AnnounceController {
+    @GetMapping("/admin/card")
+    public String admincard(){
+        return "/announce/admin/card";
+    }
     @GetMapping("/admin/list")
     public String adminList(){
         return "/announce/admin/list";
     }
-
-
     @GetMapping("/admin/writeForm")
     public String writeForm(){
         return "/announce/admin/writeForm";
     }
-
+    @GetMapping("/admin/detail")
+    public String detail(){
+        return "/announce/admin/detail";
+    }
 
 }
 
