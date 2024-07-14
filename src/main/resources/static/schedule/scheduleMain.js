44d56e9e71e8c92ddf4c68f6721f0a09c95896db
MindySo <mindyso7151@gmail.com>
2024-07-15 00:44:19 +0900

    SMJ-calendarPage_풀캘린더api_적용

--- src/main/resources/static/schedule/scheduleMain.js
+++ src/main/resources/static/schedule/scheduleMain.js
@@ -1,22 +1,41 @@
-// $(document).ready(function() {
-//     $('.row-selected-teacher').hide();
-//     console.log("done");
-//     // 초기 로드 시 체크된 라디오 버튼에 해당하는 div를 표시
-//     var initialSelectedValue = $('input[name="teacherStatusRd"]:checked').val();
-//     if (initialSelectedValue) {
-//         $('#teacherStatus' + initialSelectedValue + 'Div').show();
-//         console.log("done");
-//     }
-//
-//
-//     $('input[name="teacherStatusRd"]').on('change', function() {
-//         var selectedValue = $('input[name="teacherStatusRd"]:checked').val();
-//         console.log(selectedValue);
-//
-//         // 모든 상태 div 숨기기
-//         $('.row-selected-teacher').hide();
-//
-//         // 선택된 값에 따라 해당 div 보이기
-//         $('#teacherStatus' + selectedValue + 'Div').show();
-//     });
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        googleCalendarApiKey: "AIzaSyAKURukCy6rYdcfKAFsNYhY6wpn7XLzRqA",
+        initialView: 'dayGridMonth',
+        navLinks: false,
+        headerToolbar: {
+            left: 'prev',
+            center: 'title',
+            right: 'next'
+        },
+
+        dateClick: function (info) {
+            var today = new Date();
+            var clickedDate = new Date(info.dateStr);
+
+            var command = '';
+
+            if (clickedDate.toDateString() === today.toDateString()) {
+                command = 'detailToday';
+            } else if (clickedDate < today) {
+                command = 'detailBefore';
+            } else {
+                command = 'detailAfter';
+            }
+
+
+            eventSources :[
+                {
+                    googleCalendarId : '98c47f0c7451cb19bde6641ac44c20dcf4b9a03a83b7c8490d5c830024f1fbb1@group.calendar.google.com'
+                    , color: 'white'   // an option!
+                    , textColor: 'red' // an option!
+                }
+            ],
+
+                // window.location.href = 'schedule.do?cmd=' + cmd + '&date=' + info.dateStr + '&classNo=1&attendanceInsertStatus=0';
+                window.location.href = '/schedule/admin/' + command;
+        }
+    });
+    calendar.render();
+});
