863bb0336eeb4e7c613fc82e46d6390c9ff26525
jisoo <ji931229@gmail.com>
2024-07-14 23:44:08 +0900

    HJS-공지사항_유저_메인_구현

--- src/main/resources/static/announce/detail.js
+++ src/main/resources/static/announce/detail.js
@@ -1,8 +1,5 @@
-var deleteButton = document.getElementById('deleteButton');
+var deleteBtn = document.querySelector('.deleteBtn');
 
-// 삭제 버튼 클릭 시 경고창을 띄우는 함수를 정의합니다.
-deleteButton.addEventListener('click', function() {
-    // 경고창을 띄웁니다.
+deleteBtn.addEventListener('click', function() {
     alert('정말 삭제하시겠습니까?');
-    // 여기에 삭제 동작을 추가하려면 추가적인 코드를 작성할 수 있습니다.
 });
