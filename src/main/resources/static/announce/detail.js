2a49df95343a51bc7074bb8360001eef82ee83a4
jisoo <ji931229@gmail.com>
2024-07-15 00:33:36 +0900

    HJS-공지_디테일_수정

--- src/main/resources/static/announce/detail.js
+++ src/main/resources/static/announce/detail.js
@@ -3,3 +3,10 @@ var deleteBtn = document.querySelector('.deleteBtn');
 deleteBtn.addEventListener('click', function() {
     alert('정말 삭제하시겠습니까?');
 });
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
