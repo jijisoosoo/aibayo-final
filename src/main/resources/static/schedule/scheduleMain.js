92735bf09822e1c157bd53efcb8dad423d823e06
SMJ <mindyso7151@gmail.com>
2024-07-15 17:48:25 +0900

    SMJ-scheduleMain-날짜별_일정_조회

--- src/main/resources/static/schedule/scheduleMain.js
+++ src/main/resources/static/schedule/scheduleMain.js
@@ -1,65 +1,103 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
+
+    // 선택한 날짜가 유지되도록
+    var selectedValue;
+
     var calendarEl = document.getElementById('calendar');
+
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        googleCalendarApiKey: "AIzaSyAKURukCy6rYdcfKAFsNYhY6wpn7XLzRqA",
-        initialView: 'dayGridMonth',
-        navLinks: false,
+        themeSystem: 'bootstrap5',
         headerToolbar: {
             left: 'prev',
+            // center: 'title today',
             center: 'title',
             right: 'next'
+            // right: 'dayGridMonth'
+        },
+        dayMaxEvents: true,
+        buttonText: {
+            today: '이번달',
+            month: '월간',
+            week: '주간',
+            day: '일간',
+            list: '목록'
         },
 
-        dateClick: function (info) {
-            var today = new Date();
-            var clickedDate = new Date(info.dateStr);
-            console.log(clickedDate);
+        dateClick: function(dateInfo) {
+            //날짜 선택
+            selectedValue = dateInfo.dateStr;
 
-            var command = '';
+            // 모든 상태 div 숨기기
+            $('.single-schedule').hide();
 
-            // if (clickedDate.toDateString() === today.toDateString()) {
-            //     command = 'detailToday';
-            // } else if (clickedDate < today) {
-            //     command = 'detailBefore';
-            // } else {
-            //     command = 'detailAfter';
-            // }
+            // 선택된 값에 따라 해당 div 보이기
+            $('.' + selectedValue).show();
+            var selectedValueStr = selectedValue.replace('-','년 ').replace('-','월 ').concat('일');
+            document.getElementById("selectedDate").innerHTML = selectedValueStr;
+        },
 
 
-            eventSources :[
-                {
-                    googleCalendarId : '98c47f0c7451cb19bde6641ac44c20dcf4b9a03a83b7c8490d5c830024f1fbb1@group.calendar.google.com'
-                    , color: 'white'   // an option!
-                    , textColor: 'red' // an option!
-                }
-            ],
+        locale: 'ko',
+        datesSet: function (dateInfo) {
+            // 처음 calendar 로드 시 모든 div 숨기고
+            $('.single-schedule').hide();
 
-                // window.location.href = 'schedule.do?cmd=' + cmd + '&date=' + info.dateStr + '&classNo=1&attendanceInsertStatus=0';
-                window.location.href = '/schedule/admin/' + command;
-        }
+            // 오늘 날짜 설정
+            var initialSelectedValue = calendar.getDate().toISOString().substring(0, 10);
+            var initialSelectedValueStr = initialSelectedValue.replace('-','년 ').replace('-','월 ').concat('일');
+
+            if (selectedValue) {    // 선택한 날짜가 있는 경우
+                $('.' + selectedValue).show();
+                var selectedValueStr = selectedValue.replace('-','년 ').replace('-','월 ').concat('일');
+                document.getElementById("selectedDate").innerHTML = selectedValueStr;
+            }else if(initialSelectedValue) {    //선택한 날짜가 없는 경우 오늘 날짜의 일정 출력
+                $('.' + initialSelectedValue).show();
+                document.getElementById("selectedDate").innerHTML = initialSelectedValueStr;
+            }
+
+            // 달력에 표시 중인 달 확인
+            var currentDate = dateInfo.view.currentStart;
+            var currentMonth = currentDate.getMonth() + 1;
+            console.log("현재 표시중인 일: " + currentDate);
+            console.log("현재 표시중인 달: " + currentMonth);
+
+            // datesRender 이벤트가 발생할 때 버튼 스타일을 변경하고 보이도록 설정
+            $('.fc-toolbar button').each(function() {
+                $(this).removeClass('btn-primary')
+                    .addClass('btn-ab');
+            });
+
+            // 'dayGridMonth' 버튼을 비활성화
+            var dayGridMonthButton = document.querySelector('.fc-dayGridMonth-button');
+            if (dayGridMonthButton) {
+                dayGridMonthButton.classList.add('disabled');
+            }
+        },
+        events: [
+            {
+                title: '학부모 심폐소생술 교육',
+                start: '2024-07-15',
+                classNo: 0,
+                contents: '유치원 3층 대강당에서 학부모 심폐소생술 교육을 실시합니다.',
+                location: ''
+            },
+            {
+                title: '여름 피크닉',
+                start: '2024-07-15',
+                classNo: 0,
+                contents: '종로 3가 탑골공원으로 현장학습 진행 예정입니다.',
+                location: ''
+            },
+            {
+                title: '화채 만들기',
+                start: '2024-07-16',
+                classNo: 0,
+                contents: '각 반마다 화채 만들기 실습이 있을 예정입니다.',
+                location: ''
+            }
+        ]
     });
+
     calendar.render();
-});
 
-$(document).ready(function() {
-    $('.testdiv').hide();
-    console.log("done");
-    // 초기 로드 시 체크된 라디오 버튼에 해당하는 div를 표시
-    var initialSelectedValue = $('input[name="testRd"]:checked').val();
-    if (initialSelectedValue) {
-        $('#' + initialSelectedValue).show();
-        console.log("done");
-    }
-
-
-    $('input[name="teacherStatusRd"]').on('change', function() {
-        var selectedValue = $('input[name="testRd"]:checked').val();
-        console.log(selectedValue);
-
-        // 모든 상태 div 숨기기
-        $('.testdiv').hide();
-
-        // 선택된 값에 따라 해당 div 보이기
-        $('#' + selectedValue).show();
-    });
 });
