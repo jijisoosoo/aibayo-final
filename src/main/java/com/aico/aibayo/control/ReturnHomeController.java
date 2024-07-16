62186297a578d6c05f358c8b7e0f002f9d0229a1
JS <ji931229@gmail.com>
2024-07-16 17:48:14 +0900

    HJS-귀가동의서_메인_어드민_구현중

---
+++ src/main/java/com/aico/aibayo/control/ReturnHomeController.java
@@ -0,0 +1,38 @@
+package com.aico.aibayo.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+    @Controller
+    @RequestMapping("/retrunhome")
+    public class ReturnHomeController {
+        @GetMapping("/admin/card")
+        public String admincard(){
+            return "/returnHome/admin/card";
+        }
+        @GetMapping("/admin/list")
+        public String adminlist(){
+            return "/returnHome/admin/list";
+        }
+        @GetMapping("/admin/detail")
+        public String admindetail(){
+            return "/returnHome/admin/detail";
+        }
+        @GetMapping("/user/writeform")
+        public String write() {
+            return "/returnHome/user/writeForm";
+        }
+        @GetMapping("/user/detail")
+        public String userdetail(){
+            return "/returnHome/user/detail";
+        }
+        @GetMapping("/user/card")
+        public String usercard(){
+            return "/returnHome/user/card";
+        }
+        @GetMapping("/user/list")
+        public String userlist(){
+            return "/returnHome/user/list";
+        }
+}
