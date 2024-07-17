90482c24b287c52dd15e51c8cf096eaa6e123846
KSY <winternrabbit@gmail.com>
2024-07-17 16:47:57 +0900

    KSY-enum 추가 및 queryDSL 설정

---
+++ src/main/java/com/aico/aibayo/entity/BooleanEnum.java
@@ -0,0 +1,25 @@
+package com.aico.aibayo.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum BooleanEnum {
+    TRUE("1"),
+    FALSE("0");
+
+    private String bool;
+
+    BooleanEnum(String bool) {
+        this.bool = bool;
+    }
+
+    static BooleanEnum findByValue(String bool) {
+        for (BooleanEnum b : BooleanEnum.values()) {
+            if (bool.equals(b.bool)) {
+                return b;
+            }
+        }
+
+        throw new IllegalArgumentException("일치하는 값이 존재하지 않습니다.");
+    }
+}
