2b52cf83ab81f42c746ded8c8992a258dffde9ff
JS <ji931229@gmail.com>
2024-07-17 11:43:04 +0900

    HJS-entity_작성

---
+++ src/main/java/com/aico/aibayo/entity/AnnounceEntity.java
@@ -0,0 +1,28 @@
+package com.aico.aibayo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "announce")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
+public class AnnounceEntity {
+    @Id
+    @Column(name="announce_no")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long announceNo;
+    @Column(name="board_no")
+    private int boardNo;
+    @Column(name="announce_type")
+    private int announceType;
+    @Column(name="can_comment")
+    private String canComment;
+    @Column(name="announce_primary")
+    private String announcePrimary;
+}
---
+++ src/main/java/com/aico/aibayo/entity/AnnounceMemberEntity.java
@@ -0,0 +1,23 @@
+package com.aico.aibayo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "announce_member")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
+public class AnnounceMemberEntity {
+    @Id
+    @Column(name="announce_no")
+    private Long announceNo;//공지 번호
+    @Column(name="id")
+    private String id; //아이디
+    @Column(name="is_read")
+    private String isRead; //읽음상태
+}
