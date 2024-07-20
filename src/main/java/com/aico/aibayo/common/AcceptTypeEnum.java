cab09471a00ddfd94bb6672914b59e3e40dc0e61
ksy <winternrabbit@gmail.com>
2024-07-20 14:42:18 +0900

    KSY-엔티티_수정_및_enum_추가

---
+++ src/main/java/com/aico/aibayo/common/AcceptTypeEnum.java
@@ -0,0 +1,24 @@
+package com.aico.aibayo.common;
+
+public enum AcceptTypeEnum {
+    CLASS_KID(0),
+    PARENT_KID(1),
+    CLASS_TEACHER(2),
+    TEACHER_KINDER(3);
+
+    private final int type;
+
+    AcceptTypeEnum(int type) {
+        this.type = type;
+    }
+
+    static AcceptTypeEnum findByType(int type) {
+        for (AcceptTypeEnum acceptType : AcceptTypeEnum.values()) {
+            if (type == acceptType.type) {
+                return acceptType;
+            }
+        }
+
+        throw new IllegalArgumentException("일치하는 값이 존재하지 않습니다.");
+    }
+}
