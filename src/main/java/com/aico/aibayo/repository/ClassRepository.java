e9652aac620e162441614a62c02e5df1335c3314
KSY <winternrabbit@gmail.com>
2024-07-22 17:40:23 +0900

    KSY-알림장_등록_필요한_쿼리_작성중

--- src/main/java/com/aico/aibayo/repository/ClassRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aico.aibayo.repository;
-
-import com.aico.aibayo.entity.ClassEntity;
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
-
-public interface ClassRepository extends JpaAttributeConverter<ClassEntity, Integer> {
-}
---
+++ src/main/java/com/aico/aibayo/repository/classManage/ClassRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.aico.aibayo.repository.classManage;
+
+public class ClassRepositoryImpl implements ClassRepositoryCustom{
+}
