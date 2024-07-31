5c1fc20cc2c7451a7b0e859bc2737b74e0adfd99
SMJ <mindyso7151@gmail.com>
2024-07-31 17:00:30 +0900

    SMJ-리팩토링_완료

--- src/main/java/com/aico/aibayo/control/ScheduleController.java
+++ src/main/java/com/aico/aibayo/control/ScheduleController.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
-@RequestMapping("/schedule")
+@RequestMapping
 public class ScheduleController {
 
     @GetMapping("/admin/scheduleMain")
-    public String adminScheduleMain(){ return "/schedule/admin/scheduleMain"; }
+    public String adminScheduleMain(){ return "/admin/schedule/scheduleMain"; }
 
     @GetMapping("/admin/scheduleWrite")
-    public String adminScheduleWrite(){ return "/schedule/admin/scheduleWrite"; }
+    public String adminScheduleWrite(){ return "/admin/schedule/scheduleWrite"; }
 
     @GetMapping("/user/scheduleMain")
-    public String userScheduleWrite(){ return "/schedule/user/scheduleMain"; }
+    public String userScheduleWrite(){ return "/user/schedule/scheduleMain"; }
 }
