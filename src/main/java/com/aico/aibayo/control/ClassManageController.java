4e434dbfa4a34075163ed94e138257d5983ac8e0
AlexMJY <jmoon0227@gmail.com>
2024-07-31 16:53:36 +0900

    MJY-static_templates_디렉터리_분리

--- src/main/java/com/aico/aibayo/control/ClassManageController.java
+++ src/main/java/com/aico/aibayo/control/ClassManageController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/classManage")
-public class ClassController {
+public class ClassManageController {
 
     @GetMapping("/main")
-    public String main() {
-        return "classManage/main";
+    public String mainPage() {
+        return "/admin/classManage/main";
     }
 
 }
