b262bcb247f4d3a66cd7265794e11d68f3619119
MindySo <mindyso7151@gmail.com>
2024-08-05 01:00:15 +0900

    SMJ-teacherMain_반_별_조회_가능

--- src/main/java/com/aico/aibayo/control/TeacherController.java
+++ src/main/java/com/aico/aibayo/control/TeacherController.java
@@ -2,6 +2,7 @@ package com.aico.aibayo.control;
 
 import com.aico.aibayo.common.AcceptStatusEnum;
 import com.aico.aibayo.dto.ClassDto;
+import com.aico.aibayo.dto.kid.KidSearchCondition;
 import com.aico.aibayo.dto.member.MemberDto;
 import com.aico.aibayo.dto.teacher.TeacherSearchCondition;
 import com.aico.aibayo.dto.teacher.teacherDto;
@@ -15,9 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -55,10 +54,19 @@ public class TeacherController {
         return null;
     }
 
-    // 일반조회
+    // 일반 조회
     @GetMapping("/list")
     public String teacherMain(Model model) {
         TeacherSearchCondition condition = new TeacherSearchCondition();
+        return getConditionAndGoMain(model, condition);
+    }
+
+    // 반 별 조회 - condition에 classNo 추가
+    @PostMapping("/listByClass")
+    public String mainByClass(@RequestBody TeacherSearchCondition condition,
+                              Model model) {
+        log.info("search: {}", condition);
+
         return getConditionAndGoMain(model, condition);
     }
 
@@ -68,37 +76,25 @@ public class TeacherController {
         condition.setKinderNo(loginInfo.getKinderNo());
 
         condition.setAcceptStatus(AcceptStatusEnum.ACCEPT.getStatus());
-        List<teacherDto> acceptedTeacherList = teacherService.getAllByKinderNo(condition);
+        List<teacherDto> acceptedTeacherList = teacherService.getAcceptedTeacherByKinderNoAndClassNo(condition);
         model.addAttribute("acceptedTeacherList", acceptedTeacherList);
+        model.addAttribute("condition", condition);
+        log.info("ACCEPT: {}", condition);
+        log.info("ACCEPTlist: {}", acceptedTeacherList.toString());
+
+        // acceptStatus wait, invite에는 condition의 ClassNo = null로 재설정해서 addAttribute
+        condition.setClassNo(null);
 
         condition.setAcceptStatus(AcceptStatusEnum.WAIT.getStatus());
         List<teacherDto> waitingTeacherList = teacherService.getAllByKinderNo(condition);
         model.addAttribute("waitingTeacherList", waitingTeacherList);
+        log.info("WAIT: {}", condition);
+
 
         condition.setAcceptStatus(AcceptStatusEnum.INVITE.getStatus());
         List<teacherDto> invitedTeacherList = teacherService.getAllByKinderNo(condition);
         model.addAttribute("invitedTeacherList", invitedTeacherList);
-
-        List<ClassDto> classList = classService.getByKinderNo(loginInfo.getKinderNo());
-        model.addAttribute("classList", classList);
-
-        return "/admin/teacher/teacherMain";
-    }
-
-//    @GetMapping("/list")
-    public String mainByClass(Model model) {
-        TeacherSearchCondition condition = new TeacherSearchCondition();
-        return getConditionAndGoMainByClass(model, condition);
-    }
-
-    // 일반조회 - searchcondition 적용해서
-    public String getConditionAndGoMainByClass(Model model, TeacherSearchCondition condition) {
-        MemberDto loginInfo = (MemberDto)model.getAttribute("loginInfo");
-        condition.setKinderNo(loginInfo.getKinderNo());
-
-        condition.setAcceptStatus(AcceptStatusEnum.ACCEPT.getStatus());
-        List<teacherDto> acceptedTeacherList = teacherService.getAllByKinderNo(condition);
-        model.addAttribute("acceptedTeacherList", acceptedTeacherList);
+        log.info("INVITE: {}", condition);
 
         List<ClassDto> classList = classService.getByKinderNo(loginInfo.getKinderNo());
         model.addAttribute("classList", classList);
