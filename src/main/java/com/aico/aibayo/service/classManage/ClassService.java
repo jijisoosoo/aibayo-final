580387f40e34fcb81d142c73af3282606ac1f49a
AlexMJY <jmoon0227@gmail.com>
2024-08-12 14:25:04 +0900

    MJY-반관리_모달에_반이름_교사_원생_목록_select

--- src/main/java/com/aico/aibayo/service/classManage/ClassService.java
+++ src/main/java/com/aico/aibayo/service/classManage/ClassService.java
@@ -2,7 +2,10 @@ package com.aico.aibayo.service.classManage;
 
 import com.aico.aibayo.dto.ClassDto;
 import java.util.List;
+import java.util.Optional;
 
+import com.aico.aibayo.dto.classKid.ClassKidDto;
+import com.aico.aibayo.dto.classTeacher.ClassTeacherDto;
 import com.aico.aibayo.entity.ClassEntity;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +20,8 @@ public interface ClassService {
     List<ClassDto> getClassByKinderNoAndTeacherId(Long kinderNo, Long id);
 
     List<ClassEntity> getClassList(Long kinderNo);
+
+    List<ClassKidDto> getClassKid(Long classNo);
+
+    List<ClassTeacherDto> getClassTeacher(Long classNo);
 }
