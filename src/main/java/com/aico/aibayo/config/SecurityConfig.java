10201de5937f35ee59cf12fd07725b1305401453
Alex-MJY <jmoon0227@gmail.com>
2024-08-13 15:18:06 +0900

    MJY-Cors_설정추가(SpringSecurity,클라이언트_접속_포트_열기)

--- src/main/java/com/aico/aibayo/config/SecurityConfig.java
+++ src/main/java/com/aico/aibayo/config/SecurityConfig.java
@@ -27,8 +27,12 @@ import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationF
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import jakarta.servlet.http.Cookie;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -60,6 +64,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // CORS
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                CorsConfiguration configuration = new CorsConfiguration();
+
+                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+                configuration.setAllowedMethods(Collections.singletonList("*"));
+                configuration.setAllowCredentials(true);
+                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                configuration.setMaxAge(3600L);
+
+                // 이 부분에서 두 개의 헤더를 한 번에 노출
+                configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
+
+                return configuration;
+            }
+        }));
 
         // CSRF 보호 비활성화
         http.csrf(csrf -> csrf.disable());
