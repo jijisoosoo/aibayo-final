8942c885afd2d54dc0b6283ef1fe90ee9b64863a
ksy <winternrabbit@gmail.com>
2024-08-15 00:18:15 +0900

    KSY-메인_미세먼지_연동_준비

--- src/main/java/com/aico/aibayo/common/SidoInfoEnum.java
+++ src/main/java/com/aico/aibayo/common/SidoInfoEnum.java
@@ -1,36 +1,51 @@
 package com.aico.aibayo.common;
 
+import lombok.Getter;
+
+@Getter
 public enum SidoInfoEnum {
-    ALL("00","전국"),
-    SEOUL("11","서울"),
-    BUSAN("21","부산"),
-    DAEGU("22","대구"),
-    INCHEON("23","인천"),
-    GWANGJU("24","광주"),
-    DAEJEON("25","대전"),
-    ULSAN("26","울산"),
-    SEJONG("29","세종"),
-    GYEONGGI("31","경기"),
-    GANGWON("32","강원"),
-    CHUNGBUK("33","충북"),
-    CHUNGNAM("34","충남"),
-    JEOLLABUK("35","전북"),
-    JEOLLANAM("36","전남"),
-    GYEONGBUK("37","경북"),
-    GYEONGNAM("38","경남"),
-    JEJU("39","제주");
+    ALL("00", "00", "전국"),
+    SEOUL("11", "11","서울"),
+    BUSAN("21", "26", "부산"),
+    DAEGU("22", "27", "대구"),
+    INCHEON("23", "28", "인천"),
+    GWANGJU("24", "29", "광주"),
+    DAEJEON("25", "30", "대전"),
+    ULSAN("26", "31", "울산"),
+    SEJONG("29", "36", "세종"),
+    GYEONGGI("31", "41", "경기"),
+    GANGWON("32", "51", "강원"),
+    CHUNGBUK("33", "43", "충북"),
+    CHUNGNAM("34", "44", "충남"),
+    JEOLLABUK("35", "52", "전북"),
+    JEOLLANAM("36", "46", "전남"),
+    GYEONGBUK("37", "47", "경북"),
+    GYEONGNAM("38", "48", "경남"),
+    JEJU("39", "50", "제주");
 
     private final String sidoCode;
+    private final String kinderSidoCode;
     private final String sidoName;
 
-    SidoInfoEnum(String sidoCode, String sidoName) {
+    SidoInfoEnum(String sidoCode, String kinderSidoCode, String sidoName) {
         this.sidoCode = sidoCode;
+        this.kinderSidoCode = kinderSidoCode;
         this.sidoName = sidoName;
     }
 
     public static SidoInfoEnum findBySidoCode(String sidoCode) {
         for (SidoInfoEnum sidoInfo : SidoInfoEnum.values()) {
             if (sidoCode.equals(sidoInfo.sidoCode)) {
+                return sidoInfo;
+            }
+        }
+
+        throw new IllegalArgumentException("일치하는 값이 존재하지 않습니다.");
+    }
+
+    public static SidoInfoEnum findByKinderSidoCode(String kinderSidoCode) {
+        for (SidoInfoEnum sidoInfo : SidoInfoEnum.values()) {
+            if (kinderSidoCode.equals(sidoInfo.kinderSidoCode)) {
                 return sidoInfo;
             }
         }
--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    let url = "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+    let url = "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty";
     let serviceKey = "5zv6%2FkeqLoX546ceoeLoe%2BdLSxW9ljqCM8YBFhS9sEDtq%2FqUlFvw0jMsYmaQu5qeim81NC%2FEg0f%2Fxgk%2FATVI9A%3D%3D";
     let returnType = "json";
-    let numOfRows = 100;
-    let pageNo
-    =&=1&sidoName=대구&ver=1.0
+    let numOfRows = 1;
+    let stationName = "";
+    let dataTerm = "DAILY";
 });
